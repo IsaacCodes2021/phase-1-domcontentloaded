5a128f3a5d0263e49991db7d04f230f8c413385c
IsaacCodes2021 <84209008+IsaacCodes2021@users.noreply.github.com>
2021-07-03 12:50:26 -0700

    commit tree 3ed47bb9b32d4d61cba481102619f1cc76b60282

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+// Your code goes herenpm test
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('p').textContent = "This is really cool!"
+})
